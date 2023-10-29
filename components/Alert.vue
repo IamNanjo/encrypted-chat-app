dd94053930dc3bf2432bc9f54402a9dde9654d83
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-10-29 16:12:46 +0200

    Added basic alert message component

---
+++ components/Alert.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="alert"><slot /></div>
+</template>
+
+<style lang="scss">
+.alert {
+	background-color: hsl(3, 100%, 60%);
+	padding: 1em;
+	border-radius: 12px;
+	text-align: center;
+	box-shadow: 1px 1px 4px 1px black;
+	text-shadow: 2px 2px 4px black;
+}
+</style>
