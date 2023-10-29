e54fc62644c7467588d54d44c60381d12d4cc7f1
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-10-29 16:18:39 +0200

    Login and register routes for testing

---
+++ server/api/login.post.ts
@@ -0,0 +1,14 @@
+import bcrypt from "bcrypt";
+
+export default defineEventHandler(async (e) => {
+	const body = (await readBody(e)) as { username?: string; password?: string };
+
+	// Will not happen in normal use as the fields are required
+	if (!body || !("username" in body) || !("password" in body)) {
+		setResponseStatus(e, 400);
+		return await send(e, "Username or password missing from request body");
+	}
+
+	e.context.session.userId = 0;
+	return await sendRedirect(e, "/");
+});
