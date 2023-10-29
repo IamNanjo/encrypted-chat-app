9067570c18fcb43b386d5a74db42565ddfcf022a
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-10-29 16:11:38 +0200

    Added basic layout and global styles

---
+++ app.vue
@@ -0,0 +1,101 @@
+<template>
+	<NavBar />
+	<NuxtPage />
+</template>
+
+<style lang="scss">
+.page-enter-active,
+.page-leave-active {
+	transition: all 0.2s;
+}
+
+.page-enter-from,
+.page-leave-to {
+	opacity: 0;
+	filter: blur(1rem);
+}
+
+*,
+*::before,
+*::after {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+	font: inherit;
+	text-decoration: none;
+	transition-property: background-color, color;
+	transition-duration: 0.2s;
+	transition-timing-function: ease;
+}
+
+:root {
+	color-scheme: light;
+	--bg-primary: #cccccc;
+	--bg-raise: rgba(0, 0, 0, 0.25);
+	--bg-raise-1: var(--bg-primary);
+	--fg-primary: #ff6961;
+	--text-primary: black;
+	--text-alt: var(--fg-primary);
+	--ff-primary: Roboto, sans-serif;
+	--ff-mono: "JetBrains Mono", monospace;
+}
+:root.dark {
+	color-scheme: dark;
+	--bg-primary: #121212;
+	--bg-raise: rgba(255, 255, 255, 0.07);
+	--bg-raise-1: #232323;
+	--fg-primary: #ff6961;
+	--text-primary: white;
+	--text-alt: var(--fg-primary);
+	--ff-primary: Roboto, sans-serif;
+	--ff-mono: "JetBrains Mono", monospace;
+}
+
+:root .icon {
+	color: black;
+}
+:root.dark .icon {
+	color: white;
+}
+
+body {
+	position: relative;
+	background-color: var(--bg-primary);
+	color: var(--text-primary);
+	font-family: var(--ff-primary);
+	z-index: 0;
+}
+
+pre,
+code {
+	font-family: var(--ff-mono);
+}
+
+main {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+	height: 100svh;
+	padding-top: 3em;
+}
+
+a {
+	color: var(--text-primary);
+	user-select: none;
+
+	&:visited {
+		color: var(--text-primary);
+	}
+}
+
+button {
+	text-shadow: 2px 2px 4px black;
+	user-select: none;
+	cursor: pointer;
+}
+
+.raise {
+	background-color: var(--bg-raise);
+}
+</style>
