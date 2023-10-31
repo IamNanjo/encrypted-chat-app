31389a0fe6bf8a46f6e375be03e48f29418e132b
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-10-31 19:03:19 +0200

    Added basic authentication logic

--- server/api/register.post.ts
+++ server/api/register.post.ts
@@ -1,16 +1,42 @@
 import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
 
 export default defineEventHandler(async (e) => {
 	const body = (await readBody(e)) as { username?: string; password?: string };
 
 	// Will not happen in normal use as the fields are required
-	if (!body || !("username" in body) || !("password" in body)) {
+	if (
+		!body ||
+		!("username" in body) ||
+		!("password" in body) ||
+		!body.username ||
+		!body.password
+	) {
 		setResponseStatus(e, 400);
 		return await send(e, "Username or password missing from request body");
 	}
 
-	const hash = await bcrypt.hash("", 12);
+	const username = body.username.toString();
+	const password = body.password.toString();
 
-	e.context.session.userId = 0;
+	const prisma = new PrismaClient();
+
+	const userExists = !!(await prisma.user.count({
+		where: { username }
+	}));
+
+	if (userExists) {
+		setResponseStatus(e, 409);
+		return await send(e, "A user with this username already exists");
+	}
+
+	const hash = await bcrypt.hash(password, 12);
+
+	const user = await prisma.user.create({
+		data: { username, password: hash }
+	});
+
+	e.context.session.userId = user.id;
+	e.context.session.username = user.username;
 	return await sendRedirect(e, "/");
 });
