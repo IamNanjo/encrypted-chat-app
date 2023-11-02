9ab5baec3d7779917e6d5d7d96d7962222487eaa
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-11-02 22:34:42 +0200

    Chat menu fixed. Temporary bg colors

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,32 @@
-<script setup lang="ts">
-const chatMenuOpen = useChatMenu();
-
-function onResize() {
-	const width = (window as Window).innerWidth;
-
-	if (width >= 800) {
-		chatMenuOpen.value = true;
-	}
-}
-
-onMounted(() => {
-	onResize();
-	window.addEventListener("resize", onResize);
-});
-</script>
+<script setup lang="ts"></script>
 
 <template>
 	<main>
-		<ChatMenu v-if="chatMenuOpen" />
+		<ChatMenu />
+		<div class="chat"></div>
 	</main>
 </template>
 
 <style lang="scss">
+.chat {
+	display: flex;
+	background-color: var(--fg-primary);
+	flex-grow: 1;
+}
+
+.chat-menu,
+.chat {
+	height: 100%;
+	border: 1px solid currentColor;
+}
+
 @media screen and (min-width: 800px) {
 	.mobile-menu {
 		display: block;
 	}
-	aside {
+	.chat-menu {
 		display: flex;
+		flex-basis: 15em !important;
 	}
 }
 </style>
