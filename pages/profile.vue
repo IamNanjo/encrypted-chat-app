bb147f094db7277112104dc91aac1953baf78042
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-11-10 10:23:22 +0200

    Profile page template added

---
+++ pages/profile.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const auth = useAuth();
+const { data: profile, pending } = await useLazyFetch("/api/user/profile");
+
+onMounted(() => {
+	watch(auth, (newAuth) => {
+		if (!newAuth.authenticated) return navigateTo("/login");
+	});
+});
+</script>
+
+<template>
+	<main>
+		<div v-if="pending">Loading...</div>
+		<div v-else>{{ profile }}</div>
+	</main>
+</template>
+
+<style lang="scss"></style>
