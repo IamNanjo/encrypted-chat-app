d0aca51a21e25dc1eff6ff71e9b7b00f03abe368
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-11-10 10:45:31 +0200

    Basic message fetch logic

--- server/api/messages.get.ts
+++ server/api/messages.get.ts
@@ -1,5 +1,28 @@
 import { PrismaClient } from "@prisma/client";
 
 export default defineEventHandler(async (e) => {
-	return await send(e);
+	const query = getQuery(e) as { chatId?: string; key?: string };
+
+	const chatId = query.chatId?.toString();
+	const key = query.key?.toString();
+
+	if (!chatId || !key) return [];
+
+	if (!("userId" in e.context.session)) {
+		return await sendRedirect(e, "/login");
+	}
+
+	const prisma = new PrismaClient();
+
+	const messages = await prisma.message.findMany({
+		where: {
+			chat: {
+				id: chatId,
+				members: { some: { id: e.context.session.userId } }
+			},
+			device: { key }
+		}
+	});
+
+	return messages;
 });
