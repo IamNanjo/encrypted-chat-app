37f237719a12c6f798f93dc4ede5c7d8294f1c0a
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-11-23 16:30:22 +0200

    New module config added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,8 @@ export default defineNuxtConfig({
 		"@sidebase/nuxt-session",
 		"@nuxtjs/color-mode",
 		"nuxt-icon",
-		"@nuxtjs/plausible"
+		"@nuxtjs/plausible",
+		"@nuxtjs/google-fonts"
 	],
 	session: {
 		isEnabled: true,
@@ -32,6 +33,12 @@ export default defineNuxtConfig({
 		},
 		api: { methods: ["get", "delete"] }
 	},
-	colorMode: { classSuffix: "", storageKey: "theme", fallback: "dark" },
-	plausible: { apiHost: "https://plausible.nanjo.tech" }
+	colorMode: { classSuffix: "", storageKey: "theme", fallback: "dark", preference: "dark" },
+	plausible: { apiHost: "https://plausible.nanjo.tech" },
+	googleFonts: {
+		families: {
+			Roboto: [400, 700],
+			"JetBrains Mono": [400, 700]
+		}
+	}
 });
