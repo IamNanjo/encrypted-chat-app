42ccdb9fcb242b83fabd32e7eeaae21be2f92a58
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-11-24 18:21:55 +0200

    Chat fetching working

--- server/api/chats.get.ts
+++ server/api/chats.get.ts
@@ -1,5 +1,31 @@
 import { PrismaClient } from "@prisma/client";
 
 export default defineEventHandler(async (e) => {
-	return [{ id: "abcdef", name: "Chat 1" }];
+	if (!("userId" in e.context.session)) {
+		return await sendRedirect(e, "/login");
+	}
+
+	const prisma = new PrismaClient();
+
+	// await prisma.chat.deleteMany();
+
+	return prisma.chat.findMany({
+		where: { members: { some: { id: e.context.session.userId } } },
+		orderBy: { created: "desc" },
+		select: {
+			id: true,
+			members: {
+				select: {
+					id: true,
+					username: true,
+					devices: {
+						select: {
+							id: true,
+							key: true
+						}
+					}
+				}
+			}
+		}
+	});
 });
