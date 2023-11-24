708e2c8e39287875f38b9490c37a283358c5c7ae
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-11-24 18:23:04 +0200

    Improved basic template

--- server/api/messages.delete.ts
+++ server/api/messages.delete.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
 export default defineEventHandler(async (e) => {
+	if (!("userId" in e.context.session)) {
+		return await sendRedirect(e, "/login");
+	}
+
+	hooks.callHook("sse:event", "messages");
+
 	return await send(e);
 });
