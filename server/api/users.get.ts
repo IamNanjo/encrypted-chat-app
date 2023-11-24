0dc5064afe95d1477c230d3e1cf997d44ae39016
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-11-24 19:21:39 +0200

    Filter out current user

--- server/api/users.get.ts
+++ server/api/users.get.ts
@@ -1,12 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 
 export default defineEventHandler((e) => {
+	if (!("userId" in e.context.session)) {
+		return sendRedirect(e, "/login");
+	}
+
 	const prisma = new PrismaClient();
 
 	const query = getQuery(e) as { q?: string };
 
 	return prisma.user.findMany({
-		where: { username: { contains: query.q || "" } },
+		where: {
+			id: { not: e.context.session.userId },
+			username: { contains: query.q || "" }
+		},
 		select: { id: true, username: true }
 	});
 });
