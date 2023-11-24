2653edf9637faea066508fe8049bf0309faad22f
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-11-24 18:21:24 +0200

    Show everyones' id and username on the profile page

--- pages/profile.vue
+++ pages/profile.vue
@@ -2,6 +2,10 @@
 const auth = useAuth();
 const { data: profile, pending } = await useLazyFetch("/api/user/profile");
 
+const timeFormatter = new Intl.RelativeTimeFormat("en", {
+	style: "long"
+});
+
 onMounted(() => {
 	watch(auth, (newAuth) => {
 		if (!newAuth.authenticated) return navigateTo("/login");
@@ -12,7 +16,10 @@ onMounted(() => {
 <template>
 	<main>
 		<div v-if="pending">Loading...</div>
-		<div v-else>{{ profile }}</div>
+		<pre v-else-if="profile && Array.isArray(profile)">
+			{{ profile.map((p) => ({ id: p.id, username: p.username })) }}
+		</pre
+		>
 	</main>
 </template>
 
