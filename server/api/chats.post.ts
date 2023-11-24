0a847375b2ce352c065787fcd740660b70b049b3
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-11-24 18:22:37 +0200

    New chats can be created now

--- server/api/chats.post.ts
+++ server/api/chats.post.ts
@@ -1,5 +1,62 @@
 import { PrismaClient } from "@prisma/client";
 
 export default defineEventHandler(async (e) => {
-	return await send(e);
+	if (!("userId" in e.context.session)) {
+		return await sendRedirect(e, "/login");
+	}
+
+	const body = (await readBody(e)) as { user?: string } | null;
+
+	if (
+		!body ||
+		typeof body !== "object" ||
+		!("user" in body) ||
+		!body.user ||
+		!body.user.toString()
+	) {
+		setResponseStatus(e, 400);
+		return send(e, "No user selected");
+	}
+
+	if (body.user === e.context.session.userId) {
+		setResponseStatus(e, 400);
+		return send(e, "You cannot start a chat with yourself");
+	}
+
+	const prisma = new PrismaClient();
+
+	const alreadyExists = await prisma.chat.findFirst({
+		where: {
+			members: {
+				every: {
+					id: { in: [e.context.session.userId, body.user] }
+				}
+			}
+		}
+	});
+
+	if (alreadyExists) {
+		setResponseStatus(e, 409);
+		return send(e, "Chat already exists");
+	}
+
+	const chat = await prisma.chat.create({
+		data: {
+			members: {
+				connect: [{ id: e.context.session.userId }, { id: body.user }]
+			}
+		},
+		include: {
+			members: {
+				select: {
+					id: true
+				}
+			}
+		}
+	});
+
+	hooks.callHook("sse:event", "chats");
+
+	setResponseStatus(e, 201);
+	return chat;
 });
