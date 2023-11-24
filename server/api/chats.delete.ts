6a6a3e3055f27eb0211dec041e872b05bfef79d3
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-11-24 18:21:47 +0200

    Chat deletion working

--- server/api/chats.delete.ts
+++ server/api/chats.delete.ts
@@ -1,5 +1,37 @@
 import { PrismaClient } from "@prisma/client";
 
 export default defineEventHandler(async (e) => {
+	if (!("userId" in e.context.session)) {
+		return sendRedirect(e, "/login");
+	}
+
+	const body = (await readBody(e)) as { id?: string } | null;
+
+	if (!body || !body.id || typeof body.id !== "string") {
+		setResponseStatus(e, 400);
+		return send(e, "No chat ID provided");
+	}
+
+	const userId = e.context.session.userId;
+	const chatId = body.id;
+
+	const prisma = new PrismaClient();
+	let chat = null;
+
+	try {
+		chat = await prisma.chat.findUnique({ where: { id: chatId } });
+	} finally {
+		if (!chat) {
+			setResponseStatus(e, 404);
+			return send(e, "Chat not found - possibly already deleted");
+		}
+	}
+
+	await prisma.chat.delete({
+		where: { id: chatId, members: { some: { id: userId } } }
+	});
+
+	hooks.callHook("sse:event", "chats");
+
 	return await send(e);
 });
