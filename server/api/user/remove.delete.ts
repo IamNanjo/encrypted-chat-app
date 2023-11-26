89bbca60f6d3e707030993ef85720de244ef04df
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-11-26 12:16:48 +0200

    Implemented user deletion

--- server/api/user/remove.delete.ts
+++ server/api/user/remove.delete.ts
@@ -1,5 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
-export default defineEventHandler((e) => {
-    
+export default defineEventHandler(async (e) => {
+	if (!("userId" in e.context.session)) {
+		return sendRedirect(e, "/login");
+	}
+
+	const prisma = new PrismaClient();
+
+	await prisma.user.delete({ where: { id: e.context.session.userId } });
+
+	e.context.session = null;
+
+	setResponseStatus(e, 204);
+	return send(e, "User deleted");
 });
