ed7abf5c78184587480129b768656686219374c2
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-11-29 01:51:18 +0200

    Fix client side auth middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,4 +1,5 @@
 export default defineNuxtRouteMiddleware(async (to) => {
+	if (process.client) return;
 	const { session } = await useSession();
 	const authPages = ["/login", "/register"];
 	if (!("userId" in session) && !authPages.includes(to.path))
