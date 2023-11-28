3a1d89bb83d7d76f2a2f4e10db725d29c499584e
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-11-29 01:44:28 +0200

    Enable auth middleware on client as well

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,6 +1,4 @@
 export default defineNuxtRouteMiddleware(async (to) => {
-	if (process.client) return;
-
 	const { session } = await useSession();
 	const authPages = ["/login", "/register"];
 	if (!("userId" in session) && !authPages.includes(to.path))
