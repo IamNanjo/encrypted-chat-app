c6c82c24c8b1cddf1883f3cd38d20af275ce8d4c
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-11-28 22:30:10 +0200

    Removed unnecessary code

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,14 +1,6 @@
 <script setup lang="ts">
-definePageMeta({
-	middleware: ["auth"]
-});
-
 const auth = useAuth();
 const { data: profile, pending } = await useLazyFetch("/api/user/profile");
-
-const timeFormatter = new Intl.RelativeTimeFormat("en", {
-	style: "long"
-});
 
 onMounted(() => {
 	watch(auth, (newAuth) => {
@@ -20,8 +12,8 @@ onMounted(() => {
 <template>
 	<main>
 		<div v-if="pending">Loading...</div>
-		<pre v-else-if="profile && Array.isArray(profile)">
-			{{ profile.map((p) => ({ id: p.id, username: p.username })) }}
+		<pre v-else-if="profile && Array.isArray(profile)"
+			>{{ profile.map((p) => ({ id: p.id, username: p.username })) }}
 		</pre
 		>
 	</main>
