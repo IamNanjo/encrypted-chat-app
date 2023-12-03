66d55a785cd140ee83ea2d0fa45716376e8081c8
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-12-03 16:02:18 +0200

    Added user agent parsing

--- server/api/device.post.ts
+++ server/api/device.post.ts
@@ -1,4 +1,5 @@
 import { prisma } from "~/server/db";
+import { UAParser } from "ua-parser-js";
 
 export default defineEventHandler(async (e) => {
 	if (!("userId" in e.context.session)) {
@@ -21,7 +22,8 @@ export default defineEventHandler(async (e) => {
 		return await send(e, "Public key missing from the request");
 	}
 
-	const ua = getHeader(e, "User-Agent") || "Unknown device";
+	// Get device information for the device name
+	const device = new UAParser(getHeader(e, "User-Agent")).getResult();
 
 	// Delete devices that have not been used in 7 days
 	const oneWeekAgo = new Date(Date.now() - 604800000);
@@ -30,7 +32,11 @@ export default defineEventHandler(async (e) => {
 	// Add new device for the user or update the lastUsed date
 	return prisma.device.upsert({
 		where: { key: body.key },
-		create: { name: ua, key: body.key, user: { connect: { id: userId } } },
+		create: {
+			name: `${device.browser.name} - ${device.os.name}`,
+			key: body.key,
+			user: { connect: { id: userId } }
+		},
 		update: { lastUsed: new Date() }
 	});
 });
