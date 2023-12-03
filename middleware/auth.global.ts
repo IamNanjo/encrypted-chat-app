adc302fb9c4b1d8c14b0447a71d5e267cfd8649b
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-12-03 21:56:07 +0200

    Buggy middleware removed

--- middleware/auth.global.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtRouteMiddleware(async (to) => {
-	if (process.client) return;
-	const { session } = await useSession();
-	const authPages = ["/login", "/register"];
-	if (!("userId" in session) && !authPages.includes(to.path))
-		return navigateTo("/login");
-	if ("userId" in session && authPages.includes(to.path))
-		return navigateTo("/");
-});
