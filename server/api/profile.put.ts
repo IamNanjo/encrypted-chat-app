b3e234e7a952c6c770dc7a19e758e07eeb9719ee
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-12-04 00:26:09 +0200

    Username and password can now be changed

---
+++ server/api/profile.put.ts
@@ -0,0 +1,81 @@
+import { prisma } from "~/server/db";
+import bcrypt from "bcrypt";
+
+export default defineEventHandler(async (e) => {
+	if (!("userId" in e.context.session)) {
+		return await sendRedirect(e, "/login");
+	}
+
+	const body = (await readBody(e)) as {
+		username: string;
+		currentPassword: string;
+		newPassword: string;
+	};
+
+	if (!body || typeof body !== "object" || Array.isArray(body)) {
+		setResponseStatus(e, 400);
+		return "Wrong request body format";
+	}
+
+	if (!("username" in body)) {
+		setResponseStatus(e, 400);
+		return "Username cannot be empty";
+	}
+
+	const userExists = await prisma.user.count({
+		where: { username: body.username }
+	});
+
+	if (userExists) {
+		setResponseStatus(e, 409);
+		return "A user with that username already exists";
+	}
+
+	if ("newPassword" in body && !("currentPassword" in body)) {
+		setResponseStatus(e, 400);
+		return "You need to provide the current password in order to change it";
+	}
+
+	const user = await prisma.user.findUnique({
+		where: { id: e.context.session.userId }
+	});
+
+	if (!user) {
+		e.context.session = null;
+		return sendRedirect(e, "/login");
+	}
+
+	if (body.currentPassword && body.newPassword) {
+		const passwordMatches = await bcrypt.compare(
+			body.currentPassword,
+			user.password
+		);
+
+		if (!passwordMatches) {
+			setResponseStatus(e, 401);
+			return "Wrong current password";
+		}
+	}
+
+	const newHash = await bcrypt.hash(body.newPassword, 12);
+
+	return await prisma.user.update({
+		where: { id: e.context.session.userId },
+		data: {
+			username: body.username,
+			password: newHash
+		},
+		select: {
+			id: true,
+			username: true,
+			created: true,
+			devices: {
+				select: {
+					id: true,
+					name: true,
+					lastUsed: true
+				}
+			}
+		}
+	});
+});
