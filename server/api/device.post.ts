6832690f27f20b617194347669a911765b864821
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-12-04 08:53:09 +0200

    Combined back into upsert

--- server/api/device.post.ts
+++ server/api/device.post.ts
@@ -29,20 +29,15 @@ export default defineEventHandler(async (e) => {
 	const oneWeekAgo = new Date(Date.now() - 604800000);
 	await prisma.device.deleteMany({ where: { lastUsed: { lte: oneWeekAgo } } });
 
-	const deviceExists = await prisma.device.count({ where: { key: body.key } });
-
-	if (deviceExists) {
-		return prisma.device.update({
-			where: { key: body.key },
-			data: { lastUsed: new Date() }
-		});
-	} else {
-		return prisma.device.create({
-			data: {
-				name: `${device.browser.name} ${device.os.name}`,
-				key: body.key,
-				user: { connect: { id: userId } }
+	return prisma.device.upsert({
+		where: { key: body.key },
+		create: {
+			name: `${device.browser.name} ${device.os.name}`,
+			key: body.key,
+			user: {
+				connect: { id: userId }
 			}
-		});
-	}
+		},
+		update: { lastUsed: new Date() }
+	});
 });
