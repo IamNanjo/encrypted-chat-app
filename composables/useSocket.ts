06ddb4ae14fdb539b6a0568848f81a7888c11b7a
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-12-20 11:00:13 +0200

    useState for socket

---
+++ composables/useSocket.ts
@@ -0,0 +1,1 @@
+export default () => useState<WebSocket | null>(() => null);
--- plugins/websocket.client.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtPlugin(() => {
-	if (process.server) return;
-
-	const wsProtocol = window.location.protocol === "https:" ? "wss://" : "ws://";
-
-	const socket = new WebSocket(`${wsProtocol}${window.location.host}`);
-
-	return { provide: { socket } };
-});
