6442966f9f6dd5a4e882e715bb0401de268d63bb
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-05-02 12:31:50 +0300

    Fix hydration mismatch using ClientOnly and use extracted logOut

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,68 +2,66 @@
 const route = useRoute();
 const theme = useColorMode();
 const auth = useAuth();
-const socket = useSocket();
-
-async function logOut() {
-  if (socket.value) socket.value.close();
-
-  await $fetch("/api/chats", { method: "POST" });
-  auth.value = { authenticated: false };
-
-  return await navigateTo("/login");
-}
 </script>
 
 <template>
   <nav>
     <div>
-      <ChatMenuToggle v-if="route.path === '/'" tabindex="0" />
-      <NuxtLink
-        v-else-if="auth.authenticated"
-        to="/"
-        tabindex="0"
-        title="Back to app"
-        ><Icon name="material-symbols:arrow-back-rounded" size="2em"
-      /></NuxtLink>
+      <ClientOnly
+        ><ChatMenuToggle v-if="route.path === '/'" tabindex="0" />
+        <NuxtLink
+          v-else-if="auth.authenticated"
+          to="/"
+          tabindex="0"
+          title="Back to app"
+          ><Icon
+            name="material-symbols:arrow-back-rounded"
+            size="2em" /></NuxtLink
+      ></ClientOnly>
     </div>
     <DropdownMenu>
       <div class="profile-menu__toggle" tabindex="0" title="Account">
         <Icon name="material-symbols:account-circle" size="2em" />
       </div>
-      <div class="profile-menu__options">
-        <div v-if="auth.authenticated" class="no-select">
-          Logged in as {{ auth.username }}
+      <ClientOnly>
+        <div class="profile-menu__options">
+          <div v-if="auth.authenticated" class="no-select">
+            Logged in as {{ auth.username }}
+          </div>
+          <div>
+            <Icon name="material-symbols:brightness-4-rounded" size="1.5em" />
+            <select v-model="theme.preference" title="Theme selection">
+              <option value="system">System</option>
+              <option value="dark">Dark</option>
+              <option value="light">Light</option>
+            </select>
+          </div>
+          <NuxtLink to="/login" v-if="!auth.authenticated">
+            <Icon name="material-symbols:login-rounded" size="1.5em" />
+            <div>Log In</div>
+          </NuxtLink>
+          <NuxtLink to="/register" v-if="!auth.authenticated">
+            <Icon name="material-symbols:person-add-rounded" size="1.5em" />
+            <div>Sign Up</div>
+          </NuxtLink>
+          <NuxtLink to="/profile" v-if="auth.authenticated">
+            <Icon
+              name="material-symbols:manage-accounts-rounded"
+              size="1.5em"
+            />
+            <div>Profile</div>
+          </NuxtLink>
+          <div
+            v-if="auth.authenticated"
+            class="clickable"
+            @click="logOut"
+            tabindex="0"
+          >
+            <Icon name="material-symbols:logout-rounded" size="1.5em" />
+            <div>Log out</div>
+          </div>
         </div>
-        <div>
-          <Icon name="material-symbols:brightness-4-rounded" size="1.5em" />
-          <select v-model="theme.preference" title="Theme selection">
-            <option value="system">System</option>
-            <option value="dark">Dark</option>
-            <option value="light">Light</option>
-          </select>
-        </div>
-        <NuxtLink to="/login" v-if="!auth.authenticated">
-          <Icon name="material-symbols:login-rounded" size="1.5em" />
-          <div>Log In</div>
-        </NuxtLink>
-        <NuxtLink to="/register" v-if="!auth.authenticated">
-          <Icon name="material-symbols:person-add-rounded" size="1.5em" />
-          <div>Sign Up</div>
-        </NuxtLink>
-        <NuxtLink to="/profile" v-if="auth.authenticated">
-          <Icon name="material-symbols:manage-accounts-rounded" size="1.5em" />
-          <div>Profile</div>
-        </NuxtLink>
-        <div
-          v-if="auth.authenticated"
-          class="clickable"
-          @click.prevent="logOut"
-          tabindex="0"
-        >
-          <Icon name="material-symbols:logout-rounded" size="1.5em" />
-          <div>Log out</div>
-        </div>
-      </div>
+      </ClientOnly>
     </DropdownMenu>
   </nav>
 </template>
