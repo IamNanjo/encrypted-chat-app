5f71bf8056a8b6b5ab12eb08e8a56870b5309f27
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-05-02 12:29:53 +0300

    Extracted logOut function

---
+++ composables/logOut.ts
@@ -0,0 +1,13 @@
+export default async function logOut() {
+  const auth = useAuth();
+  const socket = useSocket();
+
+  if (socket.value) socket.value.close();
+
+  auth.value = { authenticated: false };
+  sessionStorage.removeItem("jwt");
+
+  await $fetch("/auth/logout", { method: "POST" });
+
+  navigateTo("/login");
+}
