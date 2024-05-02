948169f370e39cbc662de1da6606fb89968cb9fc
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-05-02 17:12:23 +0300

    Removed unnecessary return

--- server/routes/auth/logout.post.ts
+++ server/routes/auth/logout.post.ts
@@ -3,5 +3,5 @@ import { getSession } from "~/server/session";
 export default defineEventHandler(async (e) => {
   const session = await getSession(e);
   await session.clear();
-  return sendRedirect(e, "/login");
+  await sendRedirect(e, "/login");
 });
