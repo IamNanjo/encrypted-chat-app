d2331afa0ae5f638ff5ae67b9d4ec850e5b3b118
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-05-03 09:38:47 +0300

    Improved user deletion code

--- server/api/user.delete.ts
+++ server/api/user.delete.ts
@@ -1,17 +1,22 @@
 import db from "~/server/db";
-import {getSession} from "~/server/session";
+import { getSession } from "~/server/session";
 
 export default defineEventHandler(async (e) => {
- const session = await getSession(e);
+  const session = await getSession(e);
 
- if (!("userId" in session.data)) {
-   return sendRedirect(e, "/login");
- }
+  if (!("userId" in session.data)) {
+    return sendRedirect(e, "/login");
+  }
 
-  await db.user.delete({ where: { id: session.data.userId } });
-
-  e.context.session = null;
-
-  setResponseStatus(e, 204);
-  return send(e, "User deleted");
+  return await Promise.all([
+    db.user.delete({ where: { id: session.data.userId } }),
+    session.clear(),
+  ])
+    .then(() => {
+      setResponseStatus(e, 204);
+      return "User deleted";
+    })
+    .catch(() => {
+      setResponseStatus(e, 400);
+    });
 });
