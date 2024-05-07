38d317cd4cce36503869e4113a69176a08608de0
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-05-07 14:44:01 +0300

    Extract websocket code and start websocket in middleware

--- app.vue
+++ app.vue
@@ -36,55 +36,11 @@ onMounted(() => {
     }
   });
 
-  // Initialize socket (automatic restarts when closed)
   if (!socket.value) {
-    const wsProtocol =
-      window.location.protocol === "https:" ? "wss://" : "ws://";
-    const wsURL = `${wsProtocol}${window.location.host}`;
-
-    const startConnection = async () => {
-      if (
-        !socket.value ||
-        socket.value.readyState === socket.value.CLOSED ||
-        socket.value.readyState === socket.value.CLOSING
-      ) {
-        socket.value = new WebSocket(wsURL);
-        socket.value.addEventListener("close", startConnection);
-      }
-
-      if (!auth.value.authenticated) return navigateTo("/login");
-
-      const token = auth.value.token;
-
-      socket.value.addEventListener("message", async (e) => {
-        const { event, mode } = JSON.parse(e.data) as SocketMessage<any>;
-
-        if (event !== "auth" || mode !== "delete") return;
-
-        await Promise.all([
-          $fetch("/auth/logout", { method: "POST" }),
-          logOut(),
-        ]);
-
-        navigateTo("/login");
-      });
-
-      socket.value.addEventListener("open", () => {
-        socket.value!.send(
-          JSON.stringify({
-            event: "auth",
-            mode: "post",
-            data: token,
-          } as SocketMessage<string>)
-        );
-      });
-    };
-
-    if (!auth.value.authenticated) {
-      watch(auth, (newAuth) => {
-        if (newAuth) startConnection();
-      });
-    } else startConnection();
+    const unwatchAuth = watch(auth, (newAuth) => {
+      if (!newAuth.authenticated || !newAuth.token) return;
+      startWebsocketConnection().then(() => unwatchAuth());
+    });
   }
 });
 </script>
