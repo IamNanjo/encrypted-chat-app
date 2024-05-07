e0fac2a623150024ff7709566985182ae19ac563
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-05-07 15:11:58 +0300

    Reset state on log out

--- composables/logOut.ts
+++ composables/logOut.ts
@@ -1,15 +1,17 @@
 export default async function logOut() {
   const auth = useAuth();
+  const keyPair = useKeyPair();
+  const selectedChat = useChat();
   const socket = useSocket();
 
   if (socket.value) socket.value.close();
 
   auth.value = { authenticated: false };
+  keyPair.value = null;
+  selectedChat.value = null;
   sessionStorage.removeItem("jwt");
 
   await $fetch("/auth/logout", { method: "POST" });
 
-  clearNuxtData();
-  clearNuxtState();
   navigateTo("/login");
 }
