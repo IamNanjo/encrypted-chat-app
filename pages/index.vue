dc74f7746478035e8d648e6735a88d7862870dbd
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-05-09 14:51:51 +0300

    Extracted loading spinner component

--- pages/index.vue
+++ pages/index.vue
@@ -4,77 +4,14 @@ const keyPair = useKeyPair();
 </script>
 
 <template>
-  <main id="main">
-    <template
-      v-if="keyPair && auth.authenticated && auth.currentDevice !== null"
-    >
+  <main>
+    <LoadingSpinner
+      v-if="!keyPair || !auth.authenticated || !auth.currentDevice"
+      text="Loading cryptographic keypair"
+    />
+    <template v-else>
       <ChatMenu />
       <Chat />
     </template>
-    <ClientOnly v-else
-      ><main class="loading">
-        <div class="loading__text">Loading cryptographic keypair</div>
-        <div class="loading__ring">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div></main
-    ></ClientOnly>
   </main>
 </template>
-
-<style scoped lang="scss">
-// Loading
-.loading {
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-
-  &__text {
-    font-size: 1.25rem;
-    font-weight: 700;
-  }
-
-  &__ring,
-  &__ring div {
-    box-sizing: border-box;
-  }
-  &__ring {
-    display: inline-block;
-    position: relative;
-    width: 80px;
-    height: 80px;
-  }
-  &__ring div {
-    box-sizing: border-box;
-    display: block;
-    position: absolute;
-    width: 64px;
-    height: 64px;
-    margin: 8px;
-    border: 8px solid currentColor;
-    border-radius: 50%;
-    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-    border-color: currentColor transparent transparent transparent;
-  }
-  &__ring div:nth-child(1) {
-    animation-delay: -0.45s;
-  }
-  &__ring div:nth-child(2) {
-    animation-delay: -0.3s;
-  }
-  &__ring div:nth-child(3) {
-    animation-delay: -0.15s;
-  }
-}
-
-@keyframes lds-ring {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-</style>
