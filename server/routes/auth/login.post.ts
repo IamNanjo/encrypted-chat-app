37278ab320f25499b9e88cfa1016d1dd5dbc9fb4
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-05-10 19:41:21 +0300

    Use case-insensitive username filter

--- server/routes/auth/login.post.ts
+++ server/routes/auth/login.post.ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import z from "zod";
-import db, { User, eq } from "~/server/db";
+import db, { User, like } from "~/server/db";
 import { getSession, signJWT } from "~/server/session";
 
 export default defineEventHandler(async (e) => {
@@ -9,28 +9,33 @@ export default defineEventHandler(async (e) => {
     z.object({ username: z.string().min(1), password: z.string().min(1) }).parse
   );
 
-  const user = db.select().from(User).where(eq(User.username, username)).get();
+  const user = db
+    .select({ id: User.id, username: User.username, password: User.password })
+    .from(User)
+    .where(like(User.username, username))
+    .get();
 
   if (!user) {
     const hash = await bcrypt.hash(password, 12);
 
-    const userId = (await db.insert(User).values({ username, password: hash }))
-      .lastInsertRowid as number;
+    const user = db
+      .insert(User)
+      .values({ username, password: hash })
+      .returning({ userId: User.id, username: User.username })
+      .get();
 
-    const sessionData = { userId, username };
-
-    const token = signJWT(sessionData);
+    const token = signJWT(user);
     const session = await getSession(e);
 
-    await session.update(sessionData);
-    return { ...sessionData, token };
+    await session.update(user);
+    return { ...user, token };
   }
 
   if (!(await bcrypt.compare(password, user.password))) {
     return setResponseStatus(e, 401, "Incorrect password");
   }
 
-  const sessionData = { userId: user.id, username };
+  const sessionData = { userId: user.id, username: user.username };
 
   const token = signJWT(sessionData);
   const session = await getSession(e);
